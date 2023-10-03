aea53836133b0fb2a217774c27e4eda264e9af49
MingGH <1558772318@qq.com>
2023-10-03 10:31:36 +0800

    add unit test

---
+++ src/test/java/run/runnable/commontool/util/CommonUtilTest.java
@@ -0,0 +1,13 @@
+package run.runnable.commontool.util;
+
+
+/**
+ * @author Asher
+ * on 2023/10/3
+ */
+class CommonUtilTest {
+
+    void getIterableFromIterator() {
+        System.out.println("");
+    }
+}
