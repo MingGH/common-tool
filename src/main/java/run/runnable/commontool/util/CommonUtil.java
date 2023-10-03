dc00aeb243a64febe5fe91e51c20e3f2e9f31a41
MingGH <1558772318@qq.com>
2023-10-03 12:27:45 +0800

    add jsoup and fix jenkinsfile

--- src/main/java/run/runnable/commontool/util/CommonUtil.java
+++ src/main/java/run/runnable/commontool/util/CommonUtil.java
@@ -1,5 +1,13 @@
 package run.runnable.commontool.util;
 
+import jakarta.annotation.Nullable;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.springframework.util.StringUtils;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 
 /**
@@ -11,5 +19,22 @@ public class CommonUtil {
     public static <T> Iterable<T> getIterableFromIterator(Iterator<T> iterator)
     {
         return () -> iterator;
+    }
+
+
+    /**
+     * get html content by xpath
+     * @param resourceHtml
+     * @param xpath
+     * @return {@link String}
+     */
+    public static String getHtmlContent(String resourceHtml, String xpath) {
+        Document doc = Jsoup.parse(resourceHtml, "utf8");
+        if (StringUtils.hasLength(xpath)){
+            final Elements elements = doc.selectXpath(xpath);
+            return elements.html();
+        }else {
+            return doc.html();
+        }
     }
 }
